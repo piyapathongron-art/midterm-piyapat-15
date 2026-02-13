009d2985553f4368ed2ed0fd85187395d9810931
piyapathongron-art <piyapathongron@gmail.com>
2026-02-13 17:19:14 +0700

    fix-edit button

--- src/pages/TodoListPage.jsx
+++ src/pages/TodoListPage.jsx
@@ -8,10 +8,8 @@ import LoadingPage from './LoadingPage'
 function TodoListPage() {
     const [user, setUser] = useState([])
     const [newlist, setNewList] = useState({
-        content: ""
-    })
-    const [editContent,setEditContent] = useState({
-        content:"",
+        content: "",
+        isdone: ""
     })
     const [loading, setLoading] = useState(true)
     const token = useUserInfo((state) => state.token)
@@ -48,9 +46,9 @@ function TodoListPage() {
         setNewList((prev) => ({ ...prev, [name]: value }))
     }
 
-    const hdlChangeUpdate = (e) =>{
-        const {name, value} =e.target
-        setEditContent((prev)=>({...prev,[name]:value}))
+    const hdlChangeUpdate = (e) => {
+        const { name, value } = e.target
+        setEditContent((prev) => ({ ...prev, [name]: value }))
     }
 
 
@@ -83,16 +81,23 @@ function TodoListPage() {
 
     async function hdlUpdate(e) {
         e.preventDefault()
+
+        if (!newlist.content.trim()) {
+            return alert("check your input before edit")
+        }
+
         try {
             console.log(e.target.id)
-            const res = await axios.patch(`https://drive-accessible-pictures-send.trycloudflare.com/todos/15/${e.target.id}`,editContent)
+            const res = await axios.patch(`https://drive-accessible-pictures-send.trycloudflare.com/todos/15/${e.target.id}`, newlist)
             console.log(res)
+            fetchUserTodoList()
             toast.success('แก้ไขสำเร็จ')
         } catch (error) {
             console.log(error)
         }
     }
 
+    console.log(user)
 
     if (loading) {
         return (
@@ -107,7 +112,7 @@ function TodoListPage() {
 
                     {/* create new todolist */}
                     <form className="newtaskbox " onSubmit={hdlSubmitNewList} >
-                        <input type="text" placeholder='new task' name="content" onChange={hdlChange} value={newlist.content} className='w-80.5 mr-10' />
+                        <input type="text" placeholder='new task' name="content" onChange={hdlChange} value={newlist.content} className='w-80.5 mr-10 px-5 selection:bg-none ' />
                         <button className='bg-blue-400 p-1 px-3 rounded-xl mb-3 cursor-pointer active:bg-blue-600'>Add</button>
                         <hr className='text-gray-600' />
                     </form>
@@ -117,11 +122,11 @@ function TodoListPage() {
                         {user.map(el => (
                             <div className="list flex justify-between my-5" key={el.id}>
 
-                                <input type="checkbox" name="isdone" defaultChecked={el.isdone} id={el?.id} />
+                                <input type="checkbox" name="isdone" onChange={e => hdlChange(e, 'check')} defaultChecked={el.isdone} id={el?.id} />
 
                                 <form className="listcontent w-75 px-5 flex" id={el.id} onSubmit={hdlUpdate}>
-                                    <input type="text" name='content' value={el.content} onChange={hdlChangeUpdate}/>
-                                <button className='bg-gray-500 px-3 mr-5 rounded-2xl cursor-pointer ml-10 '>Edit</button>
+                                    <input type="text" name='content' value={el.content} onChange={hdlChangeUpdate} />
+                                    <button className='bg-gray-500 px-3 mr-5 rounded-2xl cursor-pointer ml-10 '>Edit</button>
                                 </form>
                                 <button className='cursor-pointer text-red-500' id={el.id} onClick={(e) => hdlDelList(e)}>X</button>
                             </div>
@@ -131,8 +136,8 @@ function TodoListPage() {
 
 
                 </div>
-                <pre className='text-white'>{JSON.stringify(user, null, 2)}</pre>
-                <pre className='text-white'>{JSON.stringify(editContent, null, 2)}</pre>
+                {/* <pre className='text-white'>{JSON.stringify(user, null, 2)}</pre>
+                <pre className='text-white'>{JSON.stringify(newlist, null, 2)}</pre> */}
             </div>
         </>
     )
